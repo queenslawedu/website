8251e7811963ca6d6d44b41d70c26a228fe03d1e
Samundra Shrestha <760855+samundra@users.noreply.github.com>
2024-10-06 19:53:56 +0545

    feat(content): remove unused styles

--- js/main.js
+++ js/main.js
@@ -9,29 +9,21 @@
 
 
 $(function() {
-    
+
     "use strict";
-    
+
     //===== Prealoder
-    
+
     $(window).on('load', function(event) {
         $('.preloader').delay(500).fadeOut(500);
     });
-    
-    
+
+
     //===== Search
-    
-    $('#search').on('click', function(){
-        $(".search-box").fadeIn(600);
-    });
-    $('.closebtn').on('click', function(){
-        $(".search-box").fadeOut(600);
-    });
-    
-    
+
     //===== Sticky
-    
-    $(window).on('scroll', function(event) {    
+
+    $(window).on('scroll', function(event) {
         var scroll = $(window).scrollTop();
         if (scroll < 245) {
             $(".navigation").removeClass("sticky");
@@ -41,48 +33,48 @@ $(function() {
             $(".navigation-3 img").attr("src", "images/logo.png");
         }
     });
-    
-    
+
+
     //===== Mobile Menu
-    
+
     $(".navbar-toggler").on('click', function() {
         $(this).toggleClass("active");
     });
-    
+
     var subMenu = $('.sub-menu-bar .navbar-nav .sub-menu');
-    
+
     if(subMenu.length) {
         subMenu.parent('li').children('a').append(function () {
             return '<button class="sub-nav-toggler"> <i class="fa fa-chevron-down"></i> </button>';
         });
-        
+
         var subMenuToggler = $('.sub-menu-bar .navbar-nav .sub-nav-toggler');
-        
+
         subMenuToggler.on('click', function() {
             $(this).parent().parent().children('.sub-menu').slideToggle();
             return false
         });
-        
+
     }
-    
-    
-    
+
+
+
     //===== Slick Slider
-    
+
         function mainSlider() {
-            
+
         var BasicSlider = $('.slider-active');
-            
+
         BasicSlider.on('init', function(e, slick) {
             var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
             doAnimations($firstAnimatingElements);
         });
-            
+
         BasicSlider.on('beforeChange', function(e, slick, currentSlide, nextSlide) {
             var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
             doAnimations($animatingElements);
         });
-            
+
         BasicSlider.slick({
             autoplay: true,
             autoplaySpeed: 10000,
@@ -114,10 +106,10 @@ $(function() {
         }
     }
     mainSlider();
-    
-    
+
+
     //===== Slick Category Slied
-    
+
     $('.category-slied').slick({
         dots: false,
         infinite: false,
@@ -154,10 +146,10 @@ $(function() {
         // instead of a settings object
         ]
     });
-    
-    
+
+
     //===== Slick Course Slied
-    
+
     $('.course-slied').slick({
         dots: false,
         infinite: true,
@@ -197,18 +189,18 @@ $(function() {
         // instead of a settings object
         ]
     });
-    
-    
+
+
     //====== Magnific Popup
-    
+
     $('.Video-popup').magnificPopup({
         type: 'iframe'
         // other options
     });
-    
-    
+
+
     //===== Slick testimonial Slied
-    
+
     $('.testimonial-slied').slick({
         dots: true,
         infinite: true,
@@ -252,10 +244,10 @@ $(function() {
         // instead of a settings object
         ]
     });
-    
-    
+
+
     //===== Slick testimonial Slied
-    
+
     $('.patnar-slied').slick({
         dots: false,
         infinite: true,
@@ -299,10 +291,10 @@ $(function() {
         // instead of a settings object
         ]
     });
-    
-    
+
+
     //===== Back to top
-    
+
     // Show or hide the sticky footer button
     $(window).on('scroll', function(event) {
         if($(this).scrollTop() > 600){
@@ -311,28 +303,28 @@ $(function() {
             $('.back-to-top').fadeOut(200)
         }
     });
-    
-    
+
+
     //Animate the scroll to yop
     $('.back-to-top').on('click', function(event) {
         event.preventDefault();
-        
+
         $('html, body').animate({
             scrollTop: 0,
         }, 1500);
     });
-    
-    
+
+
     //===== Counter Up
-    
+
     $('.counter').counterUp({
         delay: 10,
         time: 3000
     });
-    
-    
+
+
     //===== Slick testimonial Slied
-    
+
     $('.student-slied').slick({
         dots: false,
         infinite: true,
@@ -343,27 +335,27 @@ $(function() {
         slidesToScroll: 1,
         arrows: false,
     });
-    
-    
-    
+
+
+
     //===== Nice Select
-    
+
     $('select').niceSelect();
-    
-    
-    
+
+
+
     //===== Count Down
-    
+
     $('[data-countdown]').each(function() {
       var $this = $(this), finalDate = $(this).data('countdown');
       $this.countdown(finalDate, function(event) {
         $this.html(event.strftime('<div class="count-down-time"><div class="singel-count"><span class="number">%D :</span><span class="title">Days</span></div><div class="singel-count"><span class="number">%H :</span><span class="title">Hours</span></div><div class="singel-count"><span class="number">%M :</span><span class="title">Minuits</span></div><div class="singel-count"><span class="number">%S</span><span class="title">Seconds</span></div></div>'));
       });
     });
-    
-    
+
+
     //=====  Rating selection
-    
+
     $('.reviews-form').on('click', '.rate-wrapper .rate .rate-item', function() {
         var self = $(this),
             target = self.parent('.rate');
@@ -371,11 +363,11 @@ $(function() {
         target.find('.rate-item').removeClass('active');
         self.addClass('active');
     });
-        
-    
-    
+
+
+
     //===== Nice Number
-    
+
     $('input[type="number"]').niceNumber({
         // custom button text
         buttonDecrement: "<i class='fa fa-sort-asc' ></i>",
@@ -383,27 +375,27 @@ $(function() {
 
     });
 
-    
-    
+
+
     //===== Magnific Popup
-    
+
     $('.shop-items').magnificPopup({
       type: 'image',
       gallery:{
         enabled:true
       }
     });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
